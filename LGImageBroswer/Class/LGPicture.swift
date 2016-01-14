3d08a3c1a0eec24b321b68110b73b063f28cded3
ligang <544043963@qq.com>
2016-01-14 14:40:49 +0800

    complete basic function

---
+++ LGImageBroswer/Class/LGPicture.swift
@@ -0,0 +1,13 @@
+//
+//  LGPicture.swift
+//  LGImageBroswer
+//
+//  Created by ligang on 16/1/13.
+//  Copyright © 2016年 L&G. All rights reserved.
+//
+
+import UIKit
+
+class LGPicture: NSObject {
+
+}
